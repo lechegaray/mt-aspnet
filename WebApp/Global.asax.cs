513ada81a0637ac8e4a1b171d54839cd26b0b8f8
Luis Echegaray <luis.echegaray@kingsmensoftware.com>
2016-05-13 22:27:49 -0400

    added EF, migrations, switched to using local IIS

--- WebApp/Global.asax.cs
+++ WebApp/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebApp.Models;
 
 namespace WebApp
 {
@@ -18,6 +19,49 @@ namespace WebApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            using (var context = new MultiTenantContext())
+            {
+                context.Speakers.Add(new Speaker()
+                {
+                    LastName = Guid.NewGuid().ToString()
+                });
+                context.Sessions.Add(new Models.Session()
+                {
+                    Title = Guid.NewGuid().ToString()
+                });
+                context.SaveChanges();
+            }
+
+            //using (var context = new MultiTenantContext())
+            //{
+            //    var tenants = new List<Tenant>
+            //{
+            //    new Tenant()
+            //    {
+            //        Name = "SVCC",
+            //        DomainName = "www.silconvalley-codecamp.com",
+            //        Id = 1,
+            //        Default = true
+            //    },
+            //    new Tenant()
+            //    {
+            //        Name = "ANGU",
+            //        DomainName = "www.angularu.com",
+            //        Id = 3,
+            //        Default = false
+            //    },
+            //    new Tenant()
+            //    {
+            //        Name = "CSSC",
+            //        DomainName = "codestarssummit.com",
+            //        Id = 2,
+            //        Default = false
+            //    }
+            //};
+            //    context.Tenants.AddRange(tenants);
+            //    context.SaveChanges();
+            //}
         }
     }
 }
